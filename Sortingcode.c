22cd6ca2ec4f46a1b2c5cdb3954367e2cf72f125
Yash Shah <42682370+Optimus28@users.noreply.github.com>
2018-10-08 01:03:09 +0530

    Create Sortingcode.c

---
+++ Sortingcode.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+void main()
+{
+int i,n;
+printf("Enter the number of Elements :- ");
+scanf("%d",&n);
+
+int a[n];
+
+printf("\nEnter the numbers :- ");
+for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+printf("\n");
+
+for(i=0;i<n;i++)
+{
+    printf("%d ",a[i]);
+}
+int temp,j=0;
+
+for(i=0;i<n;i++)
+{
+    for(j=i+1;j<n;j++)
+    {
+        if(a[i]>a[j])
+        {
+            temp=a[i];
+            a[i]=a[j];
+            a[j]=temp;
+
+        }
+    }
+}
+printf("\n");
+for(i=0;i<n;i++)
+{
+    printf("%d ",a[i]);
+}
+}
